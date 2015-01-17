edc8486b9db0cb9c27ffd405d8b406cfcb07a072
fummy <fummy8080@gmail.com>
2015-01-17 22:22:56 +0900

    ssl

--- src/main/java/fummy/jokebot/bot/JokeBot.java
+++ src/main/java/fummy/jokebot/bot/JokeBot.java
@@ -81,9 +81,9 @@ public class JokeBot {
 
 
     try {
-      //HttpClient httpclient = getNewHttpClient();
+      HttpClient httpclient = getNewHttpClient();
       //HttpClient httpclient = getNewHttpClient2();
-      HttpClient httpclient = createHttpClientFactoryV43();
+      //HttpClient httpclient = createHttpClientFactoryV43();
       
       //HttpClient httpclient = new DefaultHttpClient();
       HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpclient);
