61f6f6390e7351b947f43c81844ec256b412bbf1
fummy <fummy8080@gmail.com>
2015-01-18 19:06:17 +0900

    ssl

--- src/main/java/fummy/jokebot/rest/JokeBotController.java
+++ src/main/java/fummy/jokebot/rest/JokeBotController.java
@@ -1,9 +1,7 @@
 package fummy.jokebot.rest;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
